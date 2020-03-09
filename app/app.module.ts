ec78739e5052a288c876707c6b659f6dc905658d
Akhil Ravindran <imakhilravindran@gmail.com>
2020-03-09 13:40:18 +0530

    added base componets

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { MainBannerComponent } from './components/main-banner/main-banner.component';
+import { FeaturedProductComponent } from './components/featured-product/featured-product.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SliderComponent,
+    MainBannerComponent,
+    FeaturedProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
