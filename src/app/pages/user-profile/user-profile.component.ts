adb057be80f0fc8550ea9efc433c6be6306f5bcb
Akhil Ravindran <imakhilravindran@gmail.com>
2020-07-12 19:39:03 +0530

    user profile implemented

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,25 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
-import { CommonService } from 'src/app/common.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { CommonService } from "src/app/common.service";
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  selector: "app-user-profile",
+  templateUrl: "./user-profile.component.html",
+  styleUrls: ["./user-profile.component.scss"],
 })
 export class UserProfileComponent implements OnInit {
-
   formMode: boolean = true;
+  user: any;
 
   constructor(private auth: AuthService, private common: CommonService) {
-    console.log(this.auth.getUser())
+    this.common.setLoader(true);
+    this.auth.getUser().then((res) => {
+      this.user = res.data;
+      this.common.setLoader(false);
+      console.log(this.user)
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  editProfile() : void{
+  editProfile(): void {
     this.formMode = this.formMode ? false : true;
   }
-
 }
