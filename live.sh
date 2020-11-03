6067022bb05e29537ce14da2311d4b0073d85adf
Akhil Ravindran <imakhilravindran@gmail.com>
2020-11-03 13:48:10 +0530

    update live.sh

--- live.sh
+++ live.sh
@@ -1,5 +1,5 @@
 ng build --prod
 
-aws s3 sync dist/acreativity/ s3://assertivecreativity-frontend/
+aws s3 sync dist/acreativity/ s3://assertivecreativity/
 
-xdg-open http://assertivecreativity-frontend.s3-website.us-east-2.amazonaws.com/
+xdg-open http://assertivecreativity.s3-website.us-east-2.amazonaws.com/
