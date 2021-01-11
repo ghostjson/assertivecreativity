ce0202bd56badef03701360dc5a68d1c2c48798d
Allen Francis <lallenfrancisl@gmail.com>
2021-01-11 20:36:04 +0530

    login page refresh

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -3,11 +3,13 @@ import { AuthService } from "../../services/auth.service";
 import { Router } from "@angular/router";
 import { CommonService } from "src/app/common.service";
 import { Token } from 'src/app/models/Token';
+import { MessageService } from "primeng/api";
 
 @Component({
   selector: "app-signin",
   templateUrl: "./signin.component.html",
   styleUrls: ["./signin.component.scss"],
+  providers:[MessageService]
 })
 export class SigninComponent implements OnInit {
   user: { email: string; password: string } = {
@@ -22,7 +24,8 @@ export class SigninComponent implements OnInit {
   constructor(
     private _auth: AuthService,
     private _router: Router,
-    private _common: CommonService
+    private _common: CommonService,
+    private _messageService: MessageService
   ) {}
 
   ngOnInit(): void {
@@ -39,23 +42,38 @@ export class SigninComponent implements OnInit {
     this._common.setLoader(false);
   }
 
+  /**
+   * Sign in the user
+   */
   login() {
     this._common.setLoader(true);
+    this.error = null;
 
-    try {
-      this._auth.authenticate(
-        this.user.email,
-        this.user.password
-      )
-      .subscribe((token: Token) => {
-        console.log('dingin from sign in component');
+    this._auth.authenticate(
+      this.user.email,
+      this.user.password
+    )
+    .subscribe(
+      (token: Token) => {
+        console.log('Attempting to signin from signin component');
+      },
+      (e: any) => {
+        console.error('error caught from the login function: ', e);
+        this.error = e.error.status;
+        this._common.setLoader(false);
+        this._messageService
+          .add(
+            {
+              severity:'error',
+              summary:'Login Failed',
+              detail:'Something went wrong. Kindly try again',
+              life: 30000
+            }
+          );
+      },
+      () => {
         this._router.navigate([this.returnUrl]);
-      })
-    } catch (e) {
-      console.log(e);
-      this.error = e.error.status;
-    } finally {
-      this._common.setLoader(false);
-    }
+      }
+    )
   }
 }
