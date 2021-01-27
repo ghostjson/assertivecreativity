4d18010a705a11bbc48fbef082dcf720e815132b
Allen Francis <lallenfrancisl@gmail.com>
2021-01-28 03:31:44 +0530

    add children input to radio button question maker and bug fixes on admin forms

--- src/app/components/admin-forms-question-maker-radio/admin-forms-question-maker-radio.component.ts
+++ src/app/components/admin-forms-question-maker-radio/admin-forms-question-maker-radio.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
+import { FormQuestionEvent } from 'src/app/models/OrderMailForm';
 import { AdminOrdersFormMakerService } from 'src/app/services/admin-orders-form-maker.service';
 
 @Component({
@@ -9,6 +10,8 @@ import { AdminOrdersFormMakerService } from 'src/app/services/admin-orders-form-
 })
 export class AdminFormsQuestionMakerRadioComponent {
   @Input() question: FormGroup;
+  @Output() childQuestionActive: EventEmitter<FormQuestionEvent> = new EventEmitter<FormQuestionEvent>();
+  currentChildQuestion: FormGroup;
 
   constructor(private _formMakerService: AdminOrdersFormMakerService) { }
 
@@ -32,5 +35,58 @@ export class AdminFormsQuestionMakerRadioComponent {
    */
   removeQuestionInput(inputIndex: number): void {
     this.inputs.removeAt(inputIndex);
+  }
+
+    /**
+   * get all the children inputs of an input
+   * @param inputIndex index of the parent input of the child question
+   */
+  childrenQuestions(inputIndex: number): FormArray {
+    return this.inputs.at(inputIndex).get('children_form_questions') as FormArray;
+  }
+
+  /**
+   * Add child question to the input
+   * @param inputIndex index of the input to which child question should be added
+   */
+  addChildrenQuestion(inputIndex: number): void {
+    this.currentChildQuestion = this._formMakerService.createFormQuestion(true);
+    this.childrenQuestions(inputIndex).push(this.currentChildQuestion);
+
+    // emit that child question is active
+    this.emitChildQuestion(
+      this.currentChildQuestion,
+      this.childrenQuestions(inputIndex),
+      this.childrenQuestions.length - 1
+    );
+  }
+
+  /**
+   * emit the currently active child question to parent component
+   * @param question child question currently active
+   * @param parentArray parent form array of the question
+   * @param questionIndex index of the child question in the form array
+   */
+  emitChildQuestion(question: FormGroup, parentArray: FormArray, questionIndex: number): void {
+    this.currentChildQuestion = question;
+    this.childQuestionActive.emit({
+      question: question,
+      parent: parentArray,
+      questionIndex: questionIndex
+    });
+    console.log('emit from dropdown: ', this.currentChildQuestion);
+  }
+
+  /**
+   * Emit the child question active event to the parent component
+   * @param e event
+   */
+  emitUp(e: any): void {
+    console.log('emit from dropdown: ', e);
+    this.childQuestionActive.emit({
+      question: e.question,
+      parent: e.parent,
+      questionIndex: e.questionIndex
+    });
   }
 }
