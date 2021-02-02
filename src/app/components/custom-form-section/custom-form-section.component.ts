b9c458c0a6387c6aa5c77b728c42eb0bd90174c5
Allen Francis <lallenfrancisl@gmail.com>
2021-02-03 04:54:28 +0530

    implement form progress bar updating

--- src/app/components/custom-form-section/custom-form-section.component.ts
+++ src/app/components/custom-form-section/custom-form-section.component.ts
@@ -1,40 +1,67 @@
-import { Component, ComponentFactoryResolver, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  Input,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
-import { OrderFormSectionConfig } from 'src/app/models/OrderForm';
+import {
+  OrderFormQuestionConfig,
+  OrderFormSectionConfig,
+} from 'src/app/models/OrderForm';
 import { FormComponentResolverService } from 'src/app/services/form-component-resolver/form-component-resolver.service';
 
 @Component({
   selector: 'app-custom-form-section',
   templateUrl: './custom-form-section.component.html',
-  styleUrls: ['./custom-form-section.component.scss']
+  styleUrls: ['./custom-form-section.component.scss'],
 })
 export class CustomFormSectionComponent implements OnInit {
   @Input() sectionConfig: OrderFormSectionConfig;
   @Input() sectionFormGroup: FormGroup;
 
-  @ViewChild('questionsList', {read: ViewContainerRef, static: true}) questionsList: ViewContainerRef;
+  @ViewChild('questionsList', { read: ViewContainerRef, static: true })
+  questionsList: ViewContainerRef;
 
   constructor(
     private _compFactResolver: ComponentFactoryResolver,
     private _formCompResolver: FormComponentResolverService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.questionsList.clear();
     this.sectionConfig.questions.forEach((question, questionIndex) => {
       console.log(question.type);
-      const component = this.questionsList.createComponent<any>(
-        this._compFactResolver.resolveComponentFactory(
-          this._formCompResolver.getComponent(question.type)
-        )
-      );
-
-      component.instance.questionConfig = question;
-      component.instance.questionFormGroup = this.questionEntries().at(questionIndex);
+      this.renderQuestion(question, questionIndex);
     });
   }
 
   questionEntries(): FormArray {
     return <FormArray>this.sectionFormGroup.get('questionEntries');
   }
+
+  renderQuestion(
+    config: OrderFormQuestionConfig,
+    questionIndex: number,
+    insertIndex: number = null
+  ): void {
+    const componentFactory = this._formCompResolver.getComponent(config.type);
+
+    const component = this.questionsList.createComponent<any>(
+      this._compFactResolver.resolveComponentFactory(componentFactory),
+      insertIndex
+    );
+
+    component.instance.questionConfig = config;
+    component.instance.questionFormGroup = this.questionEntries().at(
+      questionIndex
+    );
+  }
+
+  renderChildrenQuestion(
+    questionIndex: number,
+    questionConfigs: OrderFormQuestionConfig[]
+  ): void {}
 }
--- src/app/components/custom-form/custom-form.component.ts
+++ src/app/components/custom-form/custom-form.component.ts
@@ -1,8 +1,4 @@
-import {
-  Component,
-  Input,
-  OnInit
-} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
 import { CustomFormEntry, OrderFormConfig } from 'src/app/models/OrderForm';
 import { CustomFormService } from 'src/app/services/custom-form/custom-form.service';
@@ -19,8 +15,8 @@ export class CustomFormComponent implements OnInit {
 
   formGroup: FormGroup;
   activeSection: {
-    index: number,
-    formGroup: FormGroup
+    index: number;
+    formGroup: FormGroup;
   };
   formProgress: number;
 
@@ -34,39 +30,75 @@ export class CustomFormComponent implements OnInit {
 
     this.activeSection = {
       index: 0,
-      formGroup: <FormGroup>this.sectionEntries().at(0)
+      formGroup: <FormGroup>this.sectionEntries().at(0),
     };
+    this.updateFormProgress();
 
     console.log('form config: ', this.formConfig);
     console.log('formgroup created: ', this.formGroup.value);
   }
 
+  /**
+   * get section entries
+   */
   sectionEntries(): FormArray {
     return this.formGroup.get('sectionEntries') as FormArray;
   }
 
+  /**
+   * get the questions of sectionEntry at sectionIndex
+   * @param sectionIndex index of the sectionEntry
+   */
   questionEntries(sectionIndex: number): FormArray {
     return this.sectionEntries()
       .at(sectionIndex)
       .get('questionEntries') as FormArray;
   }
 
+  /**
+   * Go to next section of the form
+   */
   nextSection(): void {
     if (this.activeSection.index < (this.formConfig.sections.length - 1)) {
       this.activeSection.index += 1;
-      this.activeSection.formGroup = <FormGroup>this.sectionEntries().at(this.activeSection.index);
+      this.activeSection.formGroup = <FormGroup>(
+        this.sectionEntries().at(this.activeSection.index)
+      );
     } else {
       this.submitForm();
     }
+    console.log(this.sectionEntries().value);
+    console.log(`${this.activeSection.index} of ${this.formConfig.sections.length - 1}`);
+    this.updateFormProgress();
   }
 
+  /**
+   * Go to the previous section of the form
+   */
   prevSection(): void {
     if (this.activeSection.index > 0) {
       this.activeSection.index -= 1;
-      this.activeSection.formGroup = <FormGroup>this.sectionEntries().at(this.activeSection.index);
+      this.activeSection.formGroup = <FormGroup>(
+        this.sectionEntries().at(this.activeSection.index)
+      );
     }
+    console.log(this.sectionEntries().value);
+    console.log(`${this.activeSection.index} of ${this.formConfig.sections.length - 1}`);
+    this.updateFormProgress();
   }
 
+  /**
+   * update the progress bar value
+   */
+  updateFormProgress(): void {
+    this.formProgress = Math.floor(
+      ((this.activeSection.index + 1) / (this.formConfig.sections.length)) * 100
+    );
+  }
+
+  /**
+   * Submit the form
+   */
   submitForm(): void {
     console.log('form submitted');
   }
