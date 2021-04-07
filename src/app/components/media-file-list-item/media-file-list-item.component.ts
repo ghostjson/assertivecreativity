3166737cf1ed2ba89af8a0de409f9bcdef7998a3
Allen Francis <lallenfrancisl@gmail.com>
2021-04-07 17:02:10 +0530

    add a click to copy button for copying link in file preview and add feedback when copying link in media manager

--- src/app/components/media-file-list-item/media-file-list-item.component.ts
+++ src/app/components/media-file-list-item/media-file-list-item.component.ts
@@ -3,15 +3,19 @@ import { MenuItem } from 'primeng/api';
 import { MediaFile } from 'src/app/models/MediaManagement';
 import { AdminMediaManagerService } from 'src/app/services/admin-media-manager/admin-media-manager.service';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { pulseAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-media-file-list-item',
   templateUrl: './media-file-list-item.component.html',
   styleUrls: ['./media-file-list-item.component.scss'],
+  animations: [pulseAnimation({ duration: 250, scale: 1.05 })],
 })
 export class MediaFileListItemComponent implements OnInit {
   @Input() file: MediaFile;
   @Output() onSelect: EventEmitter<MediaFile> = new EventEmitter<MediaFile>();
+
+  copySuccess: boolean = false;
 
   menuItems: MenuItem[];
 
@@ -34,7 +38,9 @@ export class MediaFileListItemComponent implements OnInit {
       {
         label: 'Copy Link',
         command: (event: any) => {
-          this._clipboard.copy(this.file.file);
+          if (this._clipboard.copy(this.file.file)) {
+            this.copySuccess = true;
+          }
         },
       },
     ];
--- src/app/components/media-file-preview/media-file-preview.component.ts
+++ src/app/components/media-file-preview/media-file-preview.component.ts
@@ -1,10 +1,13 @@
+import { Clipboard } from '@angular/cdk/clipboard';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { pulseAnimation } from 'angular-animations';
 import { MediaFile } from 'src/app/models/MediaManagement';
 
 @Component({
   selector: 'app-media-file-preview',
   templateUrl: './media-file-preview.component.html',
   styleUrls: ['./media-file-preview.component.scss'],
+  animations: [pulseAnimation({ duration: 250, scale: 1.1 })],
 })
 export class MediaFilePreviewComponent implements OnInit {
   @Input() file: MediaFile;
@@ -12,7 +15,9 @@ export class MediaFilePreviewComponent implements OnInit {
 
   @Output() onHide: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor() {}
+  copySuccess: boolean = false;
+
+  constructor(private _clipboard: Clipboard) {}
 
   ngOnInit(): void {}
 
@@ -21,5 +26,14 @@ export class MediaFilePreviewComponent implements OnInit {
    */
   emitHide(): void {
     this.onHide.emit();
+  }
+
+  /**
+   * copy file link to the clipboard
+   */
+  copyLinkToClipboard(): void {
+    if (this._clipboard.copy(this.file.file)) {
+      this.copySuccess = true;
+    }
   }
 }
